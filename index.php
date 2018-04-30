5becc8f534a7f4f6c76f7fa4d91f78e8abdeb09c
Demerson Costa <demerson_pt@hotmail.com>
2018-04-30 02:51:34 -0300

    Adicionei busca por id e listar

--- index.php
+++ index.php
@@ -2,16 +2,26 @@
 
 	require_once("config.php");
 
-	$root = new Usuario();
-
+	//Carrega um  unico usuario
+	/*$root = new Usuario();
 	$root->loadbyId(1);
+	echo $root;*/
 
-	echo $root;
+	//Carrega varios usuarios
+	/*$lista = Usuario::getList();
+	echo json_encode($lista);*/
+
+	//Carrega uma lista de usuario buscando pelo login
+	//$search = Usuario::search("o");
+	//echo json_encode($search);
+
+	//carrega um usuario usando login e senha
+	$usuario = new Usuario();
+	$usuario->login("João","2020");
+	echo $usuario;
 
 	/*$sql = new Sql();
-
 	$usuarios = $sql->select("SELECT * FROM tb_usuario");
-
 	echo json_encode($usuarios);*/
 
 
